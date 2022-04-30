08cbefa3e854f83884a0e0c0e906fdfcc5b771d5
gaetanrm <gaetanrm@github.com>
2022-04-30 14:54:29 +0200

    mongoexport

---
+++ MongoExport/exportBase.sh
@@ -0,0 +1,4 @@
+mongoexport --db RECETTES --collection users --out Dump/users.json --jsonArray
+mongoexport --db RECETTES --collection produits --out Dump/produits.json --jsonArray
+mongoexport --db RECETTES --collection avis --out Dump/avis.json --jsonArray
+mongoexport --db RECETTES --collection recettes --out Dump/recette.json --jsonArray
